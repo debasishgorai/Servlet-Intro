9c298cf35cb472c1a4db6af25dcec1531a3bd50d
Debasish Gorai <119739769+debasishgorai@users.noreply.github.com>
2023-07-17 23:44:07 +0530

    Add files via upload

---
+++ WebXmlDemo/src/main/java/servlet/Login.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {// TODO Auto-generated method stub
+		//PrintWriter out=resp.getWriter();
+	//out.println("i am big fan of vijay");
+	System.out.println("i am dj");
+}
+}
---
+++ WebXmlDemo/src/main/java/servlet/Reg.java
@@ -0,0 +1,16 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Reg extends HttpServlet {
+	protected void sevice(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {// TODO Auto-generated method stub
+		//PrintWriter out=resp.getWriter();
+	//out.println("i am big fan of vijay");
+	System.out.println("i am djrse");
+}
+}
